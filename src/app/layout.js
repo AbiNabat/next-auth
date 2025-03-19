f54a2a1f9b3b398eed8cc5d194f817827508cfc5
AbiNabat <154244797+AbiNabat@users.noreply.github.com>
2025-03-20 02:04:00 +0300

    auth system added

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,7 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Header from "@/components/Header";
+import {ClerkLoaded, ClerkLoading, ClerkProvider} from "@clerk/nextjs";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -19,13 +20,21 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
+      <ClerkProvider>
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+      <body>
+          <ClerkLoading>
+              <div className='fixed top-0 left-0 w-screen h-screen bg-white flex justify-center items-center'>
+                  <p className='text-2xl font-bold text-gray-800'>Loading</p>
+              </div>
+          </ClerkLoading>
+        <ClerkLoaded>
       <Header />
         {children}
+        </ClerkLoaded>
       </body>
     </html>
+
+      </ClerkProvider>
   );
 }
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,8 +3,8 @@ import Link from "next/link";
 
 const Header = () => {
     return (
-        <header>
-            <div className='py-2 m-2'>
+        <header className='bg-gradient-to-r from-blue-300 to-purple-400 shadow-lg'>
+            <div className='max-w-6xl mx-auto flex justify-between items-center p-3'>
                 <Link href='/' className='text-2xl font-extrabold group cursor-pointer' >
                     <span className='text-transparent bg-clip-text bg-gradient-to-r from-blue-500 to-blue-700 drop-shadow-md group-hover:from-blue-400 group-hover:to-blue-600'>Auth</span>
                     <span className='text-transparent bg-clip-text bg-gradient-to-r from-purple-500 to-purple-700 drop-shadow-md group-hover:from-purple-400 group-hover:to-purple-600'>App</span>
@@ -14,7 +14,7 @@ const Header = () => {
                     <ul className='flex gap-4'>
                         <Link href='/'>Home</Link>
                         <Link href='/about'>About</Link>
-                        <Link href='/about'>SignIn</Link>
+                        <Link href='/sign-in'>SignIn</Link>
                     </ul>
                 </nav>
             </div>
